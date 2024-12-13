898d20d9d12ae9860989130015733b4b5c3fd3df
Bruno Tadeu Russo <brusso@maria.brunorusso.com.br>
2024-12-13 07:32:40 -0300

    update theme

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1734085926',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/certifications/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
